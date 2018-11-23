75d59a30b8a43cf3a5a2a16c769090e3136116e7
Teodora <32546710+teodora-enache@users.noreply.github.com>
2018-11-23 23:16:22 +0100

    Update Plot_kmeans15_areas_time.m

--- Plot_kmeans15_areas_time.m
+++ Plot_kmeans15_areas_time.m
@@ -17,7 +17,5 @@ title({'Water extent derived from Sentinel 1 data','September 2016-August 2018'}
 set(gcf,'Position', [489 278.6000 741.6000 483.2000]);
 legend('km^2');
 %% final step to include all months:
-xticklabels({'25/05/2017', '18/06/2017', '24/07/2017', '29/08/2017', '22/09/2017', '28/10/2017',...
-    '21/11/2017', '27/12/2017', '20/01/2018', '25/02/2018', '21/03/2018', '26/04/2018'});
 datetick('x','mm/yy','keeplimits');
 saveas(gcf,'Water_cover_evolution_kmeans15_2yrs.png');
